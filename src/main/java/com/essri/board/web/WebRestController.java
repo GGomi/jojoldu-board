516b9272347d0822e9884a276378dc0aaa4dd31a
ggomi <dhkdn4042@naver.com>
2019-02-25 22:17:18 +0900

    Added Controller & Test

--- src/main/java/com/essri/board/web/WebRestController.java
+++ src/main/java/com/essri/board/web/WebRestController.java
@@ -1,8 +1,8 @@
 package com.essri.board.web;
 
-import com.essri.board.web.domain.posts.PostsRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +12,12 @@ public class WebRestController {
 
     private PostsRepository postsRepository;
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "HelloWorld";
+    @GetMapping("/")
+    public String main() {
+        return "main";
     }
 
-    @GetMapping("/posts")
+    @PostMapping("/posts")
     public void savePosts(@RequestBody PostsSaveRequestDto dto) {
         postsRepository.save(dto.toEntity());
     }
