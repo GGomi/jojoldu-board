e6b0272a24b0c6bc961cc4ff255aadf8f46eb0c9
ggomi <dhkdn4042@naver.com>
2019-02-25 22:18:14 +0900

    Create Builder in DTO

--- src/main/java/com/essri/board/web/PostsSaveRequestDto.java
+++ src/main/java/com/essri/board/web/PostsSaveRequestDto.java
@@ -1,6 +1,6 @@
 package com.essri.board.web;
 
-import com.essri.board.web.domain.posts.Posts;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +12,13 @@ public class PostsSaveRequestDto {
     private String title;
     private String content;
     private String author;
+
+    @Builder
+    public PostsSaveRequestDto (String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
 
     public Posts toEntity() {
         return Posts.builder()
