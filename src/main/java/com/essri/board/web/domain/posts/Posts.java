4c687857c017cee732b6c3a023edf830e2df5e14
ggomi <dhkdn4042@naver.com>
2019-02-25 14:16:18 +0900

    Create Entity (Posts)

---
+++ src/main/java/com/essri/board/web/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.essri.board.web.domain.posts;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
