3b9588678ede08dbde6522533e2277afddd5a6f8
ggomi <dhkdn4042@naver.com>
2019-02-26 16:54:35 +0900

    Pollishing

--- src/main/java/com/essri/board/web/Posts.java
+++ src/main/java/com/essri/board/web/Posts.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 public class Posts extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(length = 500, nullable = false)
