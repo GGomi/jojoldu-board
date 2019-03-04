10a6f9c5853971a592a0f9f6519e376464dfb2f4
ggomi <dhkdn4042@naver.com>
2019-03-04 22:54:43 +0900

    Edit WebRestControllerTest

--- src/test/java/com/essri/board/webservice/web/WebRestControllerTest.java
+++ src/test/java/com/essri/board/webservice/web/WebRestControllerTest.java
@@ -2,6 +2,7 @@ package com.essri.board.webservice.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,6 +13,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class WebRestControllerTest {
+    @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
